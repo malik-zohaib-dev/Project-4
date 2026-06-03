04aef84f162d9944b34bccf5b0eb17642879f747
Malik Zohaib <zohaibmalik1777@gmail.com>
2026-06-03 18:22:54 +0500

    server changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -59,6 +59,8 @@ app.get("/", (req, res) => {
   res.json(foodData);
 });
 
-app.listen(9000, () => {
-  console.log("Server is running on port 9000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
